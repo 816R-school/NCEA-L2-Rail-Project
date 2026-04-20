b4a50e04f9ecd1a2e79cd05035f366df98f37a6a
816R <q3615089341@gmail.com>
2026-04-20 20:56:49 +1200

    Sprint 2: Connect Flask to SQLite and implement JOIN query

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('rail_data.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    conn = get_db_connection()
+    query = '''
+        SELECT stations.station_name, stations.description, lines.line_name, lines.color_code
+        FROM stations
+        JOIN lines ON stations.line_id = lines.line_id
+    '''
+    stations = conn.execute(query).fetchall()
+    conn.close()
+    return render_template('index.html', stations=stations)
 
 if __name__ == '__main__':
     app.run(debug=True)
