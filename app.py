1f2acc64117bb95c8e1bf19d69c7a7329d23b36f
816R <q3615089341@gmail.com>
2026-04-23 22:13:21 +1200

    update

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def home():
 @app.route('/stations')
 def stations():
     line_id = request.args.get('line')
+    search_query = request.args.get('search')
     conn = get_db_connection()
     
     query = '''
@@ -22,20 +23,38 @@ def stations():
         FROM stations s
         JOIN lines l ON s.line_id = l.line_id
         LEFT JOIN facilities f ON s.station_id = f.station_id
+        WHERE 1=1
     '''
-    
+    params = []
+
     if line_id and line_id.strip():
-        query += ' WHERE s.line_id = ?'
-        stations_data = conn.execute(query, (line_id,)).fetchall()
-    else:
-        stations_data = conn.execute(query).fetchall()
+        query += ' AND s.line_id = ?'
+        params.append(line_id)
+
+    if search_query and search_query.strip():
+        query += ' AND s.station_name LIKE ?'
+        params.append(f'%{search_query}%')
         
+    stations_data = conn.execute(query, params).fetchall()
     conn.close()
     return render_template('stations.html', stations=stations_data)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/station/<int:station_id>')
+def station_details(station_id):
+    conn = get_db_connection()
+    station = conn.execute('''
+        SELECT s.*, l.line_name, l.color_code, f.has_elevator, f.has_parking
+        FROM stations s
+        JOIN lines l ON s.line_id = l.line_id
+        LEFT JOIN facilities f ON s.station_id = f.station_id
+        WHERE s.station_id = ?
+    ''', (station_id,)).fetchone()
+    conn.close()
+    return render_template('details.html', station=station)
 
 if __name__ == '__main__':
     app.run(debug=True)
